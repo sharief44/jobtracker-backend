641ab558b905c5e03310b1ae91004b91b00bffca
Sharief Sk <sharief.sk04@gmail.com>
2026-02-24 03:31:03 +0530

    second commit

--- src/main/java/com/sharief/jobtracker/service/UserService.java
+++ src/main/java/com/sharief/jobtracker/service/UserService.java
@@ -1,20 +1,27 @@
 package com.sharief.jobtracker.service;
 
+import java.util.Optional;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
 import com.sharief.jobtracker.entity.Role;
 import com.sharief.jobtracker.entity.User;
 import com.sharief.jobtracker.repository.UserRepository;
-import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    
+    
+    public UserService(UserRepository userRepository,
+            PasswordEncoder passwordEncoder) {
+this.userRepository = userRepository;
+this.passwordEncoder = passwordEncoder;
+}
 
     public User registerUser(User user) {
 
@@ -25,6 +32,8 @@ public class UserService {
         }
 
         user.setRole(Role.USER);
+        
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
 
         return userRepository.save(user);
     }
