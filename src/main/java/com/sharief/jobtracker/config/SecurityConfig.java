dd9d9c40fb2ab1adc36d9a410e75113bc0f1d980
Sharief Sk <sharief.sk04@gmail.com>
2026-03-04 22:49:48 +0530

    updated config cors

--- src/main/java/com/sharief/jobtracker/config/SecurityConfig.java
+++ src/main/java/com/sharief/jobtracker/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.sharief.jobtracker.security.JwtAuthFilter;
 
@@ -20,22 +22,36 @@ public class SecurityConfig {
 	    }
 
 	 @Bean
-	    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	 public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-	        http
-	            .csrf(csrf -> csrf.disable())
-	            .authorizeHttpRequests(auth -> auth
-	                .requestMatchers("/api/users/register", "/api/users/login")
-	                .permitAll()
-	                .anyRequest().authenticated()
-	            )
-	            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+	     http
+	         .cors(cors -> {}) // 🔥 enable cors
+	         .csrf(csrf -> csrf.disable())
+	         .authorizeHttpRequests(auth -> auth
+	                 .requestMatchers("/api/users/register", "/api/users/login")
+	                 .permitAll()
+	                 .anyRequest().authenticated()
+	         )
+	         .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
-	        return http.build();
-	    }
+	     return http.build();
+	 }
     
     @Bean
     public org.springframework.security.crypto.password.PasswordEncoder passwordEncoder() {
         return new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder();
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
