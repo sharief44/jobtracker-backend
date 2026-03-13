c1625cc0717fe7dba2b164b29c91da0483f95c38
Sharief Sk <sharief.sk04@gmail.com>
2026-03-13 20:07:56 +0530

    updated with application properties and docker compose

---
+++ src/main/java/com/sharief/jobtracker/config/OpenApiConfig.java
@@ -0,0 +1,35 @@
+package com.sharief.jobtracker.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        return new OpenAPI()
+
+                .info(new Info()
+                        .title("Job Tracker API")
+                        .version("1.0")
+                        .description("Backend API for Job Application Tracker"))
+
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
