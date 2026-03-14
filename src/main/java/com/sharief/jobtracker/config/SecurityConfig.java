6f74b60a5a2829b5ed38872fd220572fb25dca34
Sharief Sk <sharief.sk04@gmail.com>
2026-03-15 01:36:57 +0530

    updated springconfig file with frontend url

--- src/main/java/com/sharief/jobtracker/config/SecurityConfig.java
+++ src/main/java/com/sharief/jobtracker/config/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig {
 	        		 .requestMatchers(
 	        			        "/api/users/register",
 	        			        "/api/users/login",
+	        			        "https://job-tracker-frontend-cipbolr30-shariefsk4-fools-projects.vercel.app",
 	        			        "/actuator/**",
 	        			        "/v3/api-docs/**",
 	        			        "/swagger-ui/**",
