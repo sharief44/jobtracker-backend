382ea39636ac1c22f91ae551bda2ebed5377c5cb
Sharief Sk <sharief.sk04@gmail.com>
2026-03-16 15:33:54 +0530

    added admin initializer

--- src/main/java/com/sharief/jobtracker/config/AdminInitializer.java
+++ src/main/java/com/sharief/jobtracker/config/AdminInitializer.java
@@ -1,5 +1,33 @@
 package com.sharief.jobtracker.config;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.sharief.jobtracker.entity.Role;
+import com.sharief.jobtracker.entity.User;
+import com.sharief.jobtracker.repository.UserRepository;
+
+@Configuration
 public class AdminInitializer {
 
+    @Bean
+    CommandLineRunner createAdmin(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+
+            if (userRepository.findByEmail("admin@jobtracker.com").isEmpty()) {
+
+                User admin = new User();
+                admin.setName("Admin");
+                admin.setEmail("admin@jobtracker.com");
+                admin.setPassword(passwordEncoder.encode("admin123"));
+                admin.setRole(Role.ADMIN);
+
+                userRepository.save(admin);
+
+                System.out.println("✅ Admin user created!");
+            }
+        };
+    }
 }
